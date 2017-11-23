cd45df3f45ec4706a082a9ff6998288099119500
emilyruby <zcabeme@ucl.ac.uk>
2017-11-23 18:08:02 +0000

    insert placeholders

--- app/src/algolia.js
+++ app/src/algolia.js
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch'
 
 export const algoliaClient = algoliasearch(
-  'H1LZZCXWZT',
-  '82ebbf37acf67f8717dad28efa645a16'
+  'YOUR APP_ID',
+  'YOUR (SEARCH-ONLY) API_KEY'
 )
